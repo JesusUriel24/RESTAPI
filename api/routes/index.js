81b16c571b28fca37d91de73e56b5a454f1cf53f
JesusUriel24 <solonosabes@gmail.com>
2023-06-07 07:11:00 -0600

    deploy

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 
-
 const productsRouter = require('./products.router');
 const userRouter = require('./users.router');
 const categoriesRouter = require('./categories.router');
